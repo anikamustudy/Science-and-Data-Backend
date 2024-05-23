2d7058db638819e6fdec92d6dbe2a3bd0bae4cbf
devisopen <devisopen@gmail.com>
2024-05-23 16:19:19 +0545

    api sanctum install via composer

---
+++ backend-api/app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function register()
+    {}
+
+    public function login()
+    {}
+
+    public function logout()
+    {}
+}
