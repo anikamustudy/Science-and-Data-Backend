2a815091186323ad6d83aa1c96bf8729fe64253c
devisopen <devisopen@gmail.com>
2024-05-24 23:56:11 +0545

    Authentication part

--- backend-api/app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ backend-api/app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends BaseController
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
             'password' => 'required',
-            'c_password' => 'required|same:password',
+
         ]);
 
         if ($validator->fails()) {
--- backend-api/resources/views/backend/home.blade.php
+++ backend-api/resources/views/backend/home.blade.php
@@ -4,7 +4,9 @@
 @section('content')
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="{{ route('admin.home') }}">
+                {{ config('app.name') }}
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
